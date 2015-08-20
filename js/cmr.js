da78abb9a6ac07266b0c2661d0b9eb9df2fe9f05
Cooper <cooperthomascooper@gmail.com>
2015-08-20 12:48:19 -0400

    Improved language toggle system

---
+++ js/cmr.js
@@ -0,0 +1,39 @@
+   var lang = {
+        en: {
+            EngButton: "English",
+            FrButton: "Français",
+            Title: "Interactive Forest Atlas of Cameroon",
+            SubTitle: "A practical tool for understanding and working within Cameroon's forest sector",
+            MapModule: "Explore the Map",
+            DownloadModule: "Download the Data",
+            AnalyzeModule: "Analyze the Data",
+            TutorialModule: "View the Tutorials",
+            AboutTitle: "About the Atlas",
+            AboutText1: "English about text",
+            AboutText2: "Some more English about text",
+            Partners: "A partnership between",
+            Supporters: "Supported by",
+            Share: "Share the Forest Atlas",
+            Terms: "Terms of Service",
+            Contact: "Contact us"
+          },
+
+         fr: {
+            EngButton: "Anglais",
+            FrButton: "French",
+            Title: "Atlas Interactif du Cameroun",
+            SubTitle: "Un outil pratique pour comprendre et travailler dans le secteur forestier du Cameroun",
+            MapModule: "Explorer le Plan",
+            DownloadModule: "Télécharger les Données",
+            AnalyzeModule: "Analyzer les Données",
+            TutorialModule: "Regarder les Didacticiels",
+            AboutTitle: "A propos l'atlas",
+            AboutText1: "French about text",
+            AboutText2: "Some more French about text",
+            Partners: "Un partenariat entre",
+            Supporters: "Supporté par",
+            Share: "Partager l'Atlas",
+            Terms: "Conditions d’Utilisation",
+            Contact: "Contactez nous"
+         }
+    }
--- js/main.js
+++ js/main.js
@@ -15,6 +15,24 @@ $(function() {
     });
 });
 
+function langtoggle(l){
+	if (l == 'en' ){
+		local_lang = lang.en
+	}
+	else if (l == 'fr') {
+		local_lang = lang.fr
+	}	
+	for(var key in local_lang) {
+		document.getElementById(key).innerHTML = local_lang[key]
+		//alert(local_lang[key])
+	}
+}
+    window.addEventListener("DOMContentLoaded", function() {
+        langtoggle('en');
+    }, false);
+
+
+
 // // Closes the Responsive Menu on Menu Item Click
 // $('.navbar-collapse ul li a').click(function() {
 //     $('.navbar-toggle:visible').click();
