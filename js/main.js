601fc26c7d69c3de0400c2154f55cf2ab672de73
Cooper <cooperthomascooper@gmail.com>
2015-08-21 11:37:24 -0400

    Moved icon image hover to JS

--- js/main.js
+++ js/main.js
@@ -9,6 +9,42 @@ $(function() {
     });
 });
 
+ function init() {
+ 	var image_1 = document.getElementById("map-icon");
+ 	var image_2 = document.getElementById("download-icon");
+ 	var image_3 = document.getElementById("analyze-icon");
+ 	var image_4 = document.getElementById("tutorial-icon");
+
+ 	if (!("ontouchstart" in document.documentElement) && 
+ 		! (navigator.maxTouchPoints > 0) &&
+ 		! (navigator.msMaxTouchPoints > 0) ){
+
+	image_1.addEventListener("mouseover", function(){
+ 		this.src="img/map-icon-active.svg";}, false);
+
+ 	image_1.addEventListener("mouseout", function(){
+ 		this.src="img/map-icon.svg";}, false);
+
+ 	image_2.addEventListener("mouseover", function(){
+ 		this.src="img/download-icon-active.svg";}, false);
+
+ 	image_2.addEventListener("mouseout", function(){
+ 		this.src="img/download-icon.svg";}, false);
+
+ 	image_3.addEventListener("mouseover", function(){
+ 		this.src="img/coming-soon-icon.svg";}, false);
+
+ 	image_3.addEventListener("mouseout", function(){
+ 		this.src="img/analyze-icon.svg";}, false);
+
+ 	image_4.addEventListener("mouseover", function(){
+ 		this.src="img/coming-soon-icon.svg";}, false);
+
+ 	image_4.addEventListener("mouseout", function(){
+ 		this.src="img/tutorial-icon.svg";}, false);
+ };   
+}
+
 function langtoggle(l){
 	if (l == 'en' ){
 		local_lang = lang.en
