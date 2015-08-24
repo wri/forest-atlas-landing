e4d2f89515a9cb122371eca8a0cb61e6dbc7c872
Cooper <cooperthomascooper@gmail.com>
2015-08-24 11:12:17 -0400

    Added function to set language based on URL hash

--- js/main.js
+++ js/main.js
@@ -1,61 +1,66 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
-    });
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1000, 'easeInOutExpo');
+		event.preventDefault();
+	});
 });
 
- document.addEventListener("DOMContentLoaded", iconHover, false);
+document.addEventListener("DOMContentLoaded", iconHover, false);
 
- function iconHover() {
- 	var image_1 = document.getElementById("map-icon");
- 	var image_2 = document.getElementById("download-icon");
- 	var image_3 = document.getElementById("analyze-icon");
- 	var image_4 = document.getElementById("tutorial-icon");
+function iconHover() {
+	var image_1 = document.getElementById("map-icon");
+	var image_2 = document.getElementById("download-icon");
+	var image_3 = document.getElementById("analyze-icon");
+	var image_4 = document.getElementById("tutorial-icon");
 
- 	if (!("ontouchstart" in document.documentElement) && 
- 		! (navigator.maxTouchPoints > 0) &&
- 		! (navigator.msMaxTouchPoints > 0) ){
+	if (!("ontouchstart" in document.documentElement) && 
+		! (navigator.maxTouchPoints > 0) &&
+		! (navigator.msMaxTouchPoints > 0) ){
 
-	image_1.addEventListener("mouseover", function(){
- 		this.src="img/map-icon-active.svg";}, false);
+		image_1.addEventListener("mouseover", function(){
+			this.src="img/map-icon-active.svg";}, false);
 
- 	image_1.addEventListener("mouseout", function(){
- 		this.src="img/map-icon.svg";}, false);
+	image_1.addEventListener("mouseout", function(){
+		this.src="img/map-icon.svg";}, false);
 
- 	image_2.addEventListener("mouseover", function(){
- 		this.src="img/download-icon-active.svg";}, false);
+	image_2.addEventListener("mouseover", function(){
+		this.src="img/download-icon-active.svg";}, false);
 
- 	image_2.addEventListener("mouseout", function(){
- 		this.src="img/download-icon.svg";}, false);
+	image_2.addEventListener("mouseout", function(){
+		this.src="img/download-icon.svg";}, false);
 
- 	image_3.addEventListener("mouseover", function(){
- 		this.src="img/coming-soon-icon.svg";}, false);
+	image_3.addEventListener("mouseover", function(){
+		this.src="img/coming-soon-icon.svg";}, false);
 
- 	image_3.addEventListener("mouseout", function(){
- 		this.src="img/analyze-icon.svg";}, false);
+	image_3.addEventListener("mouseout", function(){
+		this.src="img/analyze-icon.svg";}, false);
 
- 	image_4.addEventListener("mouseover", function(){
- 		this.src="img/coming-soon-icon.svg";}, false);
+	image_4.addEventListener("mouseover", function(){
+		this.src="img/coming-soon-icon.svg";}, false);
 
- 	image_4.addEventListener("mouseout", function(){
- 		this.src="img/tutorial-icon.svg";}, false);
- };   
+	image_4.addEventListener("mouseout", function(){
+		this.src="img/tutorial-icon.svg";}, false);
+};   
 };
 
+
+// Set language and update URL hash
 function langtoggle(l){
 
 	if (l == 'en' ){
 		local_lang = lang.en,
 		local_link = link.en
+		window.location.hash = "#l=en";
 	}
 	else if (l == 'fr') {
 		local_lang = lang.fr,
 		local_link = link.fr
+		window.location.hash = "#l=fr";
+
 	}	
 	for(var key in local_lang) {
 		document.getElementById(key).innerHTML = local_lang[key];
@@ -65,19 +70,18 @@ function langtoggle(l){
 	}
 }
 
-    window.addEventListener("DOMContentLoaded", function() {
-        langtoggle('en');
-    }, false);
+//Check URL hash; set appropriate page language. Defaults to English
+function loadLanguage() {
 
+	var hash = window.location.hash;
 
-        window.addEventListener("DOMContentLoaded", function() {
-        generatePartnerLogos();
-    }, false);
-
-SocialShareKit.init();
-
-
-
+	if (hash == '' || hash == '#l=en') {
+		langtoggle('en')
+	}
+	else if (hash == '#l=fr') {
+		langtoggle('fr')
+	}
+}
 
 function generatePartnerLogos() {
 
@@ -102,3 +106,9 @@ function generatePartnerLogos() {
 	}
 }
 
+window.addEventListener("DOMContentLoaded", function() {
+	loadLanguage();
+	generatePartnerLogos();
+}, false);
+
+SocialShareKit.init();
