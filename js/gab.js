f209835316253fe96120bfd5bea6f022777c3083
Cooper <cooperthomascooper@gmail.com>
2015-08-28 10:05:19 -0400

    Deleted unused files; added country-specific information

---
+++ js/gab.js
@@ -0,0 +1,116 @@
+   var lang = {
+    en: {
+        EngButton: "English",
+        FrButton: "Français",
+        Title: "Interactive Forest Atlas of Gabon",
+        SubTitle: "A practical tool for understanding and working within Gabon's forest sector",
+        MapModule: "Explore the Map",
+        DownloadModule: "Download the Data",
+        AnalyzeModule: "Analyze the Data",
+        TutorialModule: "View the Tutorials",
+        AboutTitle: "About the Atlas",
+        AboutText: "<p>The Interactive forest atlas of Gabon is a living, dynamic forest monitoring system that provides unbiased and up-to-date information on Gabon's forest sector. Built on a geographic information system (GIS) platform, the Atlas aims to strengthen forest management and land use planning by bringing information on all major land use categories onto the same standardized platform.</p><p>The underlying forest atlas database is supported and kept up-to-date by the Ministry of Forest, Environment, and Natural Resource Protection and the World Resources Institute (WRI), releasing new information as it becomes available via this <a class='bodyLink' href='http://www.gab.forest-atlas.org/map'>mapping application</a>. Other publications are released periodically and can be found in the <a class='bodyLink' href='http://gab-data.forest-atlas.or'>download section</a>.</p><p>The Interactive forest atlas of Gabon overcomes a key data challenge by integrating forest management classes with forest cover extent and change data from <a class='bodyLink' href='http://www.globalforestwatch.org/'>Global Forest Watch’s</a> near-real-time monitoring system. With a simple click, users can run complex analyses and explore the most up-to-date land cover changes within a variety of land allocations. To learn about the wide range of functionalities, visit the online tutorial.</p><p>Unless otherwise noted, Atlas data are licensed under a <a class='bodyLink' href='http://creativecommons.org/licenses/by/4.0/'>Creative Commons Attribution 4.0 International License</a>. You are free to copy and redistribute the material in any medium or format, and to transform and build upon the material for any purpose, even commercially. You must give appropriate credit, provide a link to the license, and indicate if changes were made. When displaying and citing the data, use the appropriate credit as listed for the corresponding dataset in the <a class='bodyLink' href='http://gab-data.forest-atlas.or'>download section</a>.</p>",
+        PartnerTitle: "A partnership between",
+        SupporterTitle: "Supported by",
+        Share: "Share the Atlas",
+        TermsConditions: "Terms of Service",
+        ContactUs: "Contact us"
+    },
+    
+    fr: {
+        EngButton: "English",
+        FrButton: "Français",
+        Title: "Atlas Forestier Interactif du Gabon",
+        SubTitle: "Un outil pratique pour comprendre et travailler dans le secteur forestier du Gabon",
+        MapModule: "Explorer la Carte",
+        DownloadModule: "Télécharger les Données",
+        AnalyzeModule: "Analyzer les Données",
+        TutorialModule: "Regarder les Didacticiels",
+        AboutTitle: "A Propos l'Atlas",
+        AboutText: "<p>L’Atlas forestier interactif du Gabon est un système de suivi forestier dynamique qui fournit des informations crédibles et actualisées sur le secteur forestier au Gabon. Construit sur une plateforme SIG (Système d’Information Géographique), l’Atlas a pour but de renforcer la gestion des forêts et la planification de l’utilisation des terres en réunissant sur une même plateforme standardisée les informations sur toutes les principales catégories d’utilisation des terres.</p><p>La base de données sous-jacente de l’Atlas forestier est géré et mis à jour par le Ministère de la Forêt, de l'Environnement et de la Protection des Ressources Naturelles et le World Resources Institute (WRI), apportant de nouvelles informations à travers une <a class='bodyLink' href='gab.atlas-forestier.org/map/#v=atlas&l=fr&init=y'>application cartographique</a> lorsque celles sont disponibles. D’autres publications sont produites périodiquement et peuvent être trouvés dans la <a class='bodyLink' href='http://gab-data.atlas-forestier.org'>section téléchargement</a>.</p><p>L’Atlas Forestier Interactif répond à un défi en données clés en combinant les classes de gestion des forêts avec les données sur l’étendu et les changements de la couverture forestière provenant du système de suivi en temps quasi-réel <a class='bodyLink' href='http://www.globalforestwatch.org/#googtrans(en|fr)'>Global Forest Watch</a>. Par un simple clic, les utilisateurs peuvent effectuer des analyses complexes et explorer les changements de la couverture terrestre les plus récentes au sein d’une variété d’affectation des terres. Pour apprendre la large gamme de fonctionnalités, visiter le cours en ligne.</p><p>A moins que cela soit indiqué autrement, les données de l’Atlas forestier interactif sont sous licence <a class='bodyLink' href='http://creativecommons.org/licenses/by/4.0/deed.fr'>Creative Commons Attribution 4.0 International License</a>. Vous êtes libres de copier et redistribuer le matériel sous tous les formats, et de transformer et de mettre à profit le matériel à d’autres fins, y compris commerciales. Vous donnez donner du crédit, fournir le lien vers la licence et indiquer si des modifications ont été effectuées. Lorsque vous affichez ou citez les données, utiliser le crédit approprié tel que listé pour la série de données correspondante dans la <a class='bodyLink' href='http://gab-data.atlas-forestier.org'>section de téléchargement</a>.</p>",
+        PartnerTitle: "Un partenariat entre",
+        SupporterTitle: "Supporté par",
+        Share: "Partager l'Atlas",
+        TermsConditions: "Conditions d’Utilisation",
+        ContactUs: "Contactez-nous"   
+    }
+}
+
+var flag = {
+    href: "http://www.eaux-forets.gouv.ga/",
+    src: "img/flag/flag-gab.png",
+}
+
+var bcgImage = {
+    src: "url(img/title-bg.jpg) no-repeat center center scroll"
+}
+
+var link = {
+    en: {
+        mapLink: "http://gab.forest-atlas.org/map",
+        downloadLink: "http://gab-data.forest-atlas.org"
+    },
+    fr: {
+        mapLink: "http://gab.atlas-forestier.org/map/#v=atlas&l=fr&init=y",
+        downloadLink: "http://gab-data.atlas-forestier.org"
+    }
+}
+
+var partners = {
+    gabMin: {
+        href: "http://www.eaux-forets.gouv.ga/",
+        src: "img/logo/gab-logo.png",
+        alt: "Gabon Logo"
+    },
+    wri: {
+        href: "http://www.wri.org/",
+        src: "img/logo/wri-logo.png",
+        alt: "WRI Logo"
+    }
+}
+
+var sponsors = {
+    gfw: {
+        href: "http://globalforestwatch.org/",
+        src: "img/logo/gfw-logo-bw.png",
+        srcActive: "img/logo/gfw-logo.png",
+        alt: "GFW"
+    },
+    usaid: {
+        href: "http://www.usaid.gov/",
+        src: "img/logo/usaid-logo-bw.png",
+        srcActive: "img/logo/usaid-logo.png",
+        alt: "USAID"
+    },
+    carpe: {
+        href: "http://carpe.umd.edu/",
+        src: "img/logo/carpe-logo-bw.png",
+        srcActive: "img/logo/carpe-logo.png",
+        alt: "CARPE"
+    },
+    nmfa: {
+        href: "https://www.government.nl/ministries/ministry-of-foreign-affairs",
+        src: "img/logo/nmfa-logo-bw.png",
+        srcActive: "img/logo/nmfa-logo.png",
+        alt: "Netherlands Ministry of Foreign Affairs"
+    },
+    nmce: {
+        href: "https://www.regjeringen.no/en/dep/kld/id668/",
+        src: "img/logo/nmce-logo-bw.png",
+        srcActive: "img/logo/nmce-logo.png",
+        alt: "Norway Ministry of the Environment"
+    },
+
+    hexagon: {
+        href: "http://www.hexagongeospatial.com/",
+        src: "img/logo/hexagon-logo-bw.png",
+        srcActive: "img/logo/hexagon-logo.png",
+        alt: "Hexagon Geospatial"
+    },
+    esri: {
+        href: "http://www.esri.com/",
+        src: "img/logo/esri-logo-bw.png",
+        srcActive: "img/logo/esri-logo.png",
+        alt: "Esri"
+    }
+}
