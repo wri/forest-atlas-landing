7b355572c0aba4a14f16441963dd3e7faf3bc7e3
Thomas Maschler <tmaschler@wri.org>
2016-02-04 17:43:54 -0500

    Update cmr.js

--- js/cmr.js
+++ js/cmr.js
@@ -2,14 +2,14 @@
     en: {
         EngButton: "English",
         FrButton: "Français",
-        Title: "Interactive Forest Atlas of Cameroon",
-        SubTitle: "A practical tool for understanding and working within Cameroon's forest sector",
+        Title: "Interactive Restoration Atlas of the Federal Democratic Republic of Ethiopia",
+        SubTitle: "A practical tool for scaling up tree-based landscape restoration in Ethiopia",
         MapModule: "Explore the Map",
         DownloadModule: "Download the Data",
         AnalyzeModule: "Analyze the Data",
         TutorialModule: "View the Tutorials",
         AboutTitle: "About the Atlas",
-        AboutText: "<p>The Interactive forest atlas of Cameroon is a living, dynamic forest monitoring system that provides unbiased and up-to-date information on the Cameroonian forest sector. Built on a geographic information system (GIS) platform, the Atlas aims to strengthen forest management and land use planning by bringing information on all major land use categories onto the same standardized platform.</p><p>The underlying forest atlas database is supported and kept up-to-date by the Ministry of Forestry and Wildlife (MINFOF) and the World Resources Institute (WRI), releasing new information as it becomes available via this <a class='bodyLink' href='http://www.cmr.forest-atlas.org/map'>mapping application</a>. Other publications are released periodically and can be found in the <a class='bodyLink' href='http://www.cmr-data.forest-atlas.or'>download section</a>.</p><p>The Interactive forest atlas of Cameroon overcomes a key data challenge by integrating forest management classes with forest cover extent and change data from <a class='bodyLink' href='http://www.globalforestwatch.org/'>Global Forest Watch’s</a> near-real-time monitoring system. With a simple click, users can run complex analyses and explore the most up-to-date land cover changes within a variety of land allocations. To learn about the wide range of functionalities, visit the online tutorial.</p><p>Unless otherwise noted, Atlas data are licensed under a <a class='bodyLink' href='http://www.creativecommons.org/licenses/by/4.0/'>Creative Commons Attribution 4.0 International License</a>. You are free to copy and redistribute the material in any medium or format, and to transform and build upon the material for any purpose, even commercially. You must give appropriate credit, provide a link to the license, and indicate if changes were made. When displaying and citing the data, use the appropriate credit as listed for the corresponding dataset in the <a class='bodyLink' href='http://www.cmr-data.forest-atlas.org'>download section</a>.</p>",
+        AboutText: "<p>Ethiopia has set for itself, and the world, a high bar: in its development blueprint, the Climate Resilient Green Economy (CRGE) Strategy, it aims to achieve middle-income status by 2025 while transitioning to a climate-resilient green economy. Tree-based landscape restoration is central to these goals as trees contribute not only directly to economic development and raising living standards, but to carbon mitigation and adaptation. While trees have played a significant role in many of Ethiopia's successful restoration initiatives, tree-based landscape restoration need to be scaled up if it is to help Ethiopia meet its ambitious goals by 2025.</p><p>No single institution can singlehandedly scale up tree-based landscape restoration in agricultural, pastoral, and natural landscapes as needed to regain ecological functionality and enhance standards of living across Ethiopia. Such restoration at scale will require many state and non-state actors to act in concert and mobilize additional financial and human resources.In support of the restoration movement in Ethiopia, the Ministry of Environment, Forest and Climate Change (MEFCC), with input from regional and national experts from various sectors, has been developing national tree-based landscape restoration potential maps to support national planning and coordination. Built on a geographic information system (GIS) platform, the Interactive Restoration Atlas of Ethiopia shares the results of this effort. It also provides information on forest cover extent and change data from Global Forest Watch’s near-real-time monitoring system. With a simple click, users can run spatial analyses. To learn about the method and data used in producing the restoration potential maps, the users can refer to the online technical documentation.</p><p>The underlying restoration atlas database is supported and kept up-to-date by the Ministry of Environment, Forest and Climate Change.</p><p>Unless otherwise noted, Atlas data are licensed under a Creative Commons Attribution 4.0 International License. You are free to copy and redistribute the material in any medium or format, and to transform and build upon the material for any purpose, even commercially. You must give appropriate credit, provide a link to the license, and indicate if changes were made. When displaying and citing the data, use the appropriate credit as listed for the corresponding dataset in the download section.</p>",
         PartnerTitle: "A partnership between",
         SupporterTitle: "Supported by",
         Share: "Share the Atlas",
@@ -17,28 +17,11 @@
         ContactUs: "Contact us"
     },
     
-    fr: {
-        EngButton: "English",
-        FrButton: "Français",
-        Title: "Atlas Forestier Interactif du Cameroun",
-        SubTitle: "Un outil pratique pour comprendre et travailler dans le secteur forestier du Cameroun",
-        MapModule: "Explorer la Carte",
-        DownloadModule: "Télécharger les Données",
-        AnalyzeModule: "Analyzer les Données",
-        TutorialModule: "Regarder les Didacticiels",
-        AboutTitle: "A Propos l'Atlas",
-        AboutText: "<p>L’Atlas forestier interactif du Cameroun est un système de suivi forestier dynamique qui fournit des informations crédibles et actualisées sur le secteur forestier au Cameroun. Construit sur une plateforme SIG (Système d’Information Géographique), l’Atlas a pour but de renforcer la gestion des forêts et la planification de l’utilisation des terres en réunissant sur une même plateforme standardisée les informations sur toutes les principales catégories d’utilisation des terres.</p><p>La base de données sous-jacente de l’Atlas forestier est géré et mis à jour par le Ministère des Forêts et de la Faune (MINFOF) et le World Resources Institute (WRI), apportant de nouvelles informations à travers une <a class='bodyLink' href='www.cmr.atlas-forestier.org/map/#v=atlas&l=fr&init=y'>application cartographique</a> lorsque celles sont disponibles. D’autres publications sont produites périodiquement et peuvent être trouvés dans la <a class='bodyLink' href='http://www.cmr-data.atlas-forestier.org'>section téléchargement</a>.</p><p>L’Atlas Forestier Interactif répond à un défi en données clés en combinant les classes de gestion des forêts avec les données sur l’étendu et les changements de la couverture forestière provenant du système de suivi en temps quasi-réel <a class='bodyLink' href='http://www.globalforestwatch.org/#googtrans(en|fr)'>Global Forest Watch</a>. Par un simple clic, les utilisateurs peuvent effectuer des analyses complexes et explorer les changements de la couverture terrestre les plus récentes au sein d’une variété d’affectation des terres. Pour apprendre la large gamme de fonctionnalités, visiter le cours en ligne.</p><p>A moins que cela soit indiqué autrement, les données de l’Atlas forestier interactif sont sous licence <a class='bodyLink' href='http://www.creativecommons.org/licenses/by/4.0/deed.fr'>Creative Commons Attribution 4.0 International License</a>. Vous êtes libres de copier et redistribuer le matériel sous tous les formats, et de transformer et de mettre à profit le matériel à d’autres fins, y compris commerciales. Vous donnez donner du crédit, fournir le lien vers la licence et indiquer si des modifications ont été effectuées. Lorsque vous affichez ou citez les données, utiliser le crédit approprié tel que listé pour la série de données correspondante dans la <a class='bodyLink' href='http://www.cmr-data.atlas-forestier.org'>section de téléchargement</a>.</p>",
-        PartnerTitle: "Un partenariat entre",
-        SupporterTitle: "Supporté par",
-        Share: "Partager l'Atlas",
-        TermsConditions: "Conditions d’Utilisation",
-        ContactUs: "Contactez-nous"   
-    }
 }
 
 var flag = {
-    href: "http://www.minfof.cm/",
-    src: "img/flag/flag-cameroon.png",
+    href: "Ministry of Environment, Forest and Climate Change",
+    src: "img/flag/flag-ethiopia.png",
 }
 
 var bcgImage = {
@@ -47,25 +30,30 @@ var bcgImage = {
 
 var link = {
     en: {
-        mapLink: "http://cmr.forest-atlas.org/map",
-        downloadLink: "http://cmr-data.forest-atlas.org"
+        mapLink: "http://my.forest-atlas.org/map/?appid=3e056b4e11a54e67b6f8b90c3787cf1d",
+        downloadLink: "http://eth-data.forest-atlas.org"
     },
     fr: {
-        mapLink: "http://cmr.atlas-forestier.org/map/#v=atlas&l=fr&init=y",
-        downloadLink: "http://cmr-data.atlas-forestier.org"
+        mapLink: "http://my.forest-atlas.org/map/?appid=3e056b4e11a54e67b6f8b90c3787cf1d",
+        downloadLink: "http://eth-data.atlas-forestier.org"
     }
 }
 
 var partners = {
     cmrMin: {
-        href: "http://www.minfof.cm/",
-        src: "img/logo/minfof-logo.png",
-        alt: "MINFOF Logo"
+        href: "https://www.facebook.com/MefEth/",
+        src: "img/logo/mefcc-logo.png",
+        alt: "MEFCC Logo"
     },
     wri: {
         href: "http://www.wri.org/",
         src: "img/logo/wri-logo.png",
         alt: "WRI Logo"
+    }
+    cf: {
+        href: "https://www.clintonfoundation.org/",
+        src: "img/logo/cf-logo.png",
+        alt: "CF Logo"
     }
 }
 
@@ -76,29 +64,29 @@ var sponsors = {
         srcActive: "img/logo/gfw-logo.png",
         alt: "GFW"
     },
-    usaid: {
-        href: "http://www.usaid.gov/",
-        src: "img/logo/usaid-logo-bw.png",
-        srcActive: "img/logo/usaid-logo.png",
-        alt: "USAID"
+    gef: {
+        href: "https://www.thegef.org/gef/",
+        src: "img/logo/gef-bw.png",
+        srcActive: "img/logo/gef.png",
+        alt: "Global Envionment Facility"
     },
-    ukaid: {
-        href: "https://www.gov.uk/government/organisations/department-for-international-development",
-        src: "img/logo/ukaid-logo-bw.png",
-        srcActive: "img/logo/ukaid-logo.png",
-        alt: "UKAID"
+    bmz: {
+        href: "https://www.bmz.de/en/",
+        src: "img/logo/bmz-bw.png",
+        srcActive: "img/logo/bmz.png",
+        alt: "Federal Ministry for Economic Cooperation and Development of Germany"
+    },
+    bmz: {
+        href: "http://www.bmub.bund.de/en/",
+        src: "img/logo/bmub-bw.png",
+        srcActive: "img/logo/bmub.png",
+        alt: "Federal Ministry for the Environment, Nature Conservation, Building and Nuclear Safety of Germany"
     },
     nmce: {
         href: "https://www.regjeringen.no/en/dep/kld/id668/",
         src: "img/logo/nmce-logo-bw.png",
         srcActive: "img/logo/nmce-logo.png",
         alt: "Norway Ministry of the Environment"
-    },
-    hexagon: {
-        href: "http://www.hexagongeospatial.com/",
-        src: "img/logo/hexagon-logo-bw.png",
-        srcActive: "img/logo/hexagon-logo.png",
-        alt: "Hexagon Geospatial"
     },
     esri: {
         href: "http://www.esri.com/",
