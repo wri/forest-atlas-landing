932bcb81004a931a6c27be3ac74b8843b152b2ed
Thomas Maschler <tmaschler@wri.org>
2016-02-04 19:07:05 -0500

    ethiopia

---
+++ js/lang.js
@@ -0,0 +1,97 @@
+   var lang = {
+    en: {
+        EngButton: "English",
+        FrButton: "Français",
+        Title: "Interactive Restoration Atlas of the Federal Democratic Republic of Ethiopia",
+        SubTitle: "A practical tool for scaling up tree-based landscape restoration in Ethiopia",
+        MapModule: "Explore the Map",
+        DownloadModule: "Download the Data",
+        AnalyzeModule: "Analyze the Data",
+        TutorialModule: "View the Tutorials",
+        AboutTitle: "About the Atlas",
+        AboutText: "<p>Ethiopia has set for itself, and the world, a high bar: in its development blueprint, the Climate Resilient Green Economy (CRGE) Strategy, it aims to achieve middle-income status by 2025 while transitioning to a climate-resilient green economy. Tree-based landscape restoration is central to these goals as trees contribute not only directly to economic development and raising living standards, but to carbon mitigation and adaptation. While trees have played a significant role in many of Ethiopia's successful restoration initiatives, tree-based landscape restoration need to be scaled up if it is to help Ethiopia meet its ambitious goals by 2025.</p><p>No single institution can singlehandedly scale up tree-based landscape restoration in agricultural, pastoral, and natural landscapes as needed to regain ecological functionality and enhance standards of living across Ethiopia. Such restoration at scale will require many state and non-state actors to act in concert and mobilize additional financial and human resources.In support of the restoration movement in Ethiopia, the Ministry of Environment, Forest and Climate Change (MEFCC), with input from regional and national experts from various sectors, has been developing national tree-based landscape restoration potential maps to support national planning and coordination. Built on a geographic information system (GIS) platform, the Interactive Restoration Atlas of Ethiopia shares the results of this effort. It also provides information on forest cover extent and change data from Global Forest Watch’s near-real-time monitoring system. With a simple click, users can run spatial analyses. To learn about the method and data used in producing the restoration potential maps, the users can refer to the online technical documentation.</p><p>The underlying restoration atlas database is supported and kept up-to-date by the Ministry of Environment, Forest and Climate Change.</p><p>Unless otherwise noted, Atlas data are licensed under a Creative Commons Attribution 4.0 International License. You are free to copy and redistribute the material in any medium or format, and to transform and build upon the material for any purpose, even commercially. You must give appropriate credit, provide a link to the license, and indicate if changes were made. When displaying and citing the data, use the appropriate credit as listed for the corresponding dataset in the download section.</p>",
+        PartnerTitle: "A partnership between",
+        SupporterTitle: "Supported by",
+        Share: "Share the Atlas",
+        TermsConditions: "Terms of Service",
+        ContactUs: "Contact us"
+    },
+    
+}
+
+var flag = {
+    href: "Ministry of Environment, Forest and Climate Change",
+    src: "img/flag/flag-ethiopia.gif",
+}
+
+var bcgImage = {
+    src: "url(img/title-bg-8.jpg) no-repeat center center scroll"
+}
+
+var link = {
+    en: {
+        mapLink: "http://my.forest-atlas.org/map/?appid=3e056b4e11a54e67b6f8b90c3787cf1d",
+        downloadLink: "http://eth-data.forest-atlas.org"
+    },
+    fr: {
+        mapLink: "http://my.forest-atlas.org/map/?appid=3e056b4e11a54e67b6f8b90c3787cf1d",
+        downloadLink: "http://eth-data.atlas-forestier.org"
+    }
+}
+
+var partners = {
+    cmrMin: {
+        href: "https://www.facebook.com/MefEth/",
+        src: "img/logo/mefcc-logo.png",
+        alt: "MEFCC Logo"
+    },
+    wri: {
+        href: "http://www.wri.org/",
+        src: "img/logo/wri-logo.png",
+        alt: "WRI Logo"
+    },
+    cf: {
+        href: "https://www.clintonfoundation.org/",
+        src: "img/logo/cf-logo.png",
+        alt: "CF Logo"
+    }
+}
+
+var sponsors = {
+    gfw: {
+        href: "http://globalforestwatch.org/",
+        src: "img/logo/gfw-logo-bw.png",
+        srcActive: "img/logo/gfw-logo.png",
+        alt: "GFW"
+    },
+    gef: {
+        href: "https://www.thegef.org/gef/",
+        src: "img/logo/gef-bw.png",
+        srcActive: "img/logo/gef.png",
+        alt: "Global Envionment Facility"
+    },
+    bmz: {
+        href: "https://www.bmz.de/en/",
+        src: "img/logo/bmz-bw.png",
+        srcActive: "img/logo/bmz.png",
+        alt: "Federal Ministry for Economic Cooperation and Development of Germany"
+    },
+    bmub: {
+        href: "http://www.bmub.bund.de/en/",
+        src: "img/logo/bmub-bw.png",
+        srcActive: "img/logo/bmub.png",
+        alt: "Federal Ministry for the Environment, Nature Conservation, Building and Nuclear Safety of Germany"
+    },
+    nmce: {
+        href: "https://www.regjeringen.no/en/dep/kld/id668/",
+        src: "img/logo/nmce-logo-bw.png",
+        srcActive: "img/logo/nmce-logo.png",
+        alt: "Norway Ministry of the Environment"
+    },
+    esri: {
+        href: "http://www.esri.com/",
+        src: "img/logo/esri-logo-bw.png",
+        srcActive: "img/logo/esri-logo.png",
+        alt: "Esri"
+    }
+}
